4e9bedc7f437152fcc9a2ae099814e3253b0cbca
greenerlabgithub <uihun@greener-lab.com>
2025-05-21 06:13:13 +0000

    apps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ImageAnalyzer from './components/ImageAnalyzer';  // ← 경로 주의!
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageAnalyzer />  {/* 여기서 컴포넌트를 렌더링 */}
     </div>
   );
 }
 
 export default App;
+
